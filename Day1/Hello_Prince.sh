e0c4ce0297f544cefe497e2eefd631f94a315193
Prince Efe Oruma <156837295+iamprinceefe@users.noreply.github.com>
2024-11-29 08:19:22 -0500

    Create Hello_Prince.sh

---
+++ Day1/Hello_Prince.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Greetings
+echo "Mariam: Hello Prince! How're you ?"
+
+#Reply to Greetings
+echo "Prince: Hi Mariam, i'm Great and yourself ?"
+
+# Reply from Prince
+echo "Mariam: Very well thank GOD!"
